475c37896b9aacff164e097984c9f93d600b82fb
MEGAPLASTOR <dinht7124@gmail.com>
2026-08-31 02:55:13 +0700

    fix: Loai bo dau ngoac gay loi cu phap cmd trong start-agent-windows.bat

--- agent/start-agent-windows.bat
+++ agent/start-agent-windows.bat
@@ -9,17 +9,10 @@ echo.
 
 cd /d "%~dp0"
 
-if not exist "node_modules\@nestjs\core" (
-    echo [1/2] Dang tu dong cai dat thu vien cho Agent (chi mat 10 giay)...
-    call npm install
-)
-
-echo [2/2] Dang khoi dong Agent tren Port 4001...
-echo.
-
 set AGENT_PORT=4001
 set AGENT_SECRET_KEY=agent_secret_key_123
 
+echo Dang khoi dong Agent tren Port 4001...
 node dist\main.js
 
 pause
